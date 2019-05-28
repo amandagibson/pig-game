de09107b5d9710552e1f2c01679adec14245cf74
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-05-28 15:41:31 +0200

    hides dice

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ activePlayer = 1;
 dice = Math.floor(Math.random() * 6) +1;
 
 document.querySelector('#current-' + activePlayer).textContent = dice;
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
