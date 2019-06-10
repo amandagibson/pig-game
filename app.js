c8b679aa4f17f393622f5429efa4545467680f12
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-06-10 09:12:49 +0200

    adds updated round score after dice rolls with player change if a 1 is rolled.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
+scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -16,9 +16,12 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 	var diceDOM = document.querySelector('.dice');
 	diceDOM.style.display = 'block';
 	diceDOM.src = 'dice-' + dice + '.png';
+
+	if (dice !== 1) {
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+	}
 });
-
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-
-// var x = document.querySelector('#score-0').textContent;
-// console.log(x);
